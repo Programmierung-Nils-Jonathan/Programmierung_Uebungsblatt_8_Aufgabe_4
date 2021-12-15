8431811192f3ba8c0b7f4c934c84b47bd2b781ce
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2021-12-15 17:36:45 +0000

    continued development on MySetElement

--- MySetElement.java
+++ MySetElement.java
@@ -9,4 +9,24 @@ protected class MySetElement <T>{
        this.value = pValue;
    }
 
+   public boolean contains(T compareObject) {
+       if(next == null){
+            return false;
+       } else if(value == compareObject) {
+            return true;
+       } else {
+           return next.contains();
+       }
+   }
+
+   public boolean containsAll(T[] compareObjects) {
+        for(int i = 0; i < compareObjects.length; i++) {
+            if(!this.contains(compareObjects[i])) {
+                return false;
+            }
+        }
+
+        return true;
+   }
+
 }
