f8bbfb55e6258ad2fb2fce3ee8931a7c8d573fa6
Nils Bollwerk <93703256+Nils4112@users.noreply.github.com>
2021-12-15 12:21:25 +0100

    Create mySetIterator.java

---
+++ mySetIterator.java
@@ -0,0 +1,41 @@
+package mySets;
+
+import java.util.Iterator;
+
+public class mySetIterator <T>implements java.util.Iterator{
+    mySetElement<T> current;
+    public mySetIterator(mySetElement<T> pCurrent){
+        this.current = pCurrent;
+    }
+    @Override
+    public boolean hasNext() {
+        if(current==null)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
+
+    @Override
+    public mySetElement<T> next() {
+        mySetElement<T> neu = current;
+        try{
+            current = current.next;
+        }
+        catch(java.util.NoSuchElementException e){
+            throw e;
+        }
+        return neu;
+
+    }
+
+
+    @Override
+    public void remove() {
+        throw new java.lang.UnsupportedOperationException();
+    }
+}
