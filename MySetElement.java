54390bf73c29a437a1f789e493d0addd6fc2ece7
Nils Bollwerk <93703256+Nils4112@users.noreply.github.com>
2021-12-16 17:11:38 +0100

    Update MySetElement.java

--- MySetElement.java
+++ MySetElement.java
@@ -1,76 +1,13 @@
 package mySets;
 
-protected class MySetElement <T>{
+ class MySetElement <T>{
     MySetElement <T> next;
     T value;
 
-   public MySetElement(mySetElement <T> pNext, T pValue){
-       this.next = pNext;
-       this.value = pValue;
-   }
-
-   public boolean contains(T compareObject) {
-       if(next == null){
-            return false;
-       } else if(this.value.equals(compareObject)) {
-            return true;
-       } else {
-           return next.contains();
-       }
-   }
-
-   public boolean containsAll(T[] compareObjects) {
-        for(int i = 0; i < compareObjects.length; i++) {
-            if(!this.contains(compareObjects[i])) {
-                return false;
-            }
-        }
-
-        return true;
-   }
-
-   public int size() {
-       if(next != null) {
-           if(value == null) {
-               return 0;
-           }
-           return 1;
-       } else {
-            return 1 + next.size();
-       }
-   }
-
-   public boolean isEmpty() {
-        return this.size() == 0;
-   }
-
-   String toString() {
-        if(next == null) {
-            return this.value;
-        } else {
-            return this.value + "," + this.next.toString();
-        }
-   }
-
-   void add(T addObject) {
-       if(!this.contains(addObject)) {
-            this.next = new MySetElement<MySetElement.T>(this.next, addObject) ;
-       }
-   }
-
-   void clear() {
-       this.value = null;
-       this.next = null;
-   }
-
-    void remove(T removeObject) {
-        if (this.value.equals(removeObject)) {
-            return;
-        } else if (this.next == null) {
-            return;
-        } else {
-            next.remove(removeObject);
-        }
+    public MySetElement(MySetElement <T> pNext, T pValue){
+        this.next = pNext;
+        this.value = pValue;
     }
+
 
 }
